8e692465b65c3b07929d6086f2591f2d0440e82c
natepage <nathan.page@eonx.com>
2021-03-05 00:18:15 +0000

    [EasyCore] Allow to set any ES option on search service (#489)

--- src/Bridge/Symfony/Resources/config/bugsnag_reporter.php
+++ src/Bridge/Symfony/Resources/config/bugsnag_reporter.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 use EonX\EasyErrorHandler\Bridge\BridgeConstantsInterface;
 use EonX\EasyErrorHandler\Bridge\Bugsnag\BugsnagReporterProvider;
 use EonX\EasyErrorHandler\Bridge\Bugsnag\ErrorDetailsClientConfigurator;
-use EonX\EasyErrorHandler\Bridge\Bugsnag\SeverityClientConfigurator;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
@@ -19,7 +18,5 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ->arg('$threshold', '%' . BridgeConstantsInterface::PARAM_BUGSNAG_THRESHOLD . '%')
         ->arg('$ignoredExceptions', '%' . BridgeConstantsInterface::PARAM_BUGSNAG_IGNORED_EXCEPTIONS . '%');
 
-    $services
-        ->set(ErrorDetailsClientConfigurator::class)
-        ->set(SeverityClientConfigurator::class);
+    $services->set(ErrorDetailsClientConfigurator::class);
 };
